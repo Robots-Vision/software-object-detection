4d8a096adee54eff3fc7f9c8680821213cafde43
joelee229 <joel.sena229@gmail.com>
2020-06-01 20:30:55 -0300

    Extração de características

--- src/processo.py
+++ src/processo.py
@@ -1,43 +1,56 @@
 import numpy as np
 import cv2
 
-# VideoCapture(id da cam ou caminho do vídeo)
-cap = cv2.VideoCapture(0)  # Acessa a webcam
-while True:
-    # .read() lê a webcam retornando um frame e um boolean que indica se teve sucesso ou não na requisição da webcam
-    (x, frame) = cap.read()
+for c in range(4):
+    img = cv2.imread(
+        'C:/Users/Friday/software-object-detection/assets/image'+str(c+1)+'.jpeg')
+    largura = img.shape[1]
+    altura = img.shape[0]
+    proporcao = float(altura/largura)
+    largura_nova = 300  # em pixels
+    altura_nova = int(largura_nova*proporcao)
+    tamanho_novo = (largura_nova, altura_nova)
+    img = cv2.resize(img, tamanho_novo, interpolation=cv2.INTER_AREA)
 
-    # Se for false sai do while
-    if not x:
-        break
+    # Converte para um img em tons de cinza
+    # cv2.cvtColor(<imagem>, <Tipo a converter>)
+    img_pb = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Suaviza ou borra o img e tira o ruído dele. Aumenta a precisão das bordas Sempre ímpares
+    # cv2.GaussianBlur(<imagem>, (<largura>, <altura>), <qtd de desvios no eixo X e Y>)
+    img_sv = cv2.GaussianBlur(img_pb, (3, 3), 0)
 
-    # Redimensiona o frame pela metade utilizando a técnica slicing
-    frame = frame[::2, ::2]
+    # Binariza o img fazendo um cálculo das intensidades dos imgs, onde pixels proximos de 0 se tornam 0 e os pixels proximos ao 255 ficam 255
+    # cv2.threshold(<imagem>, <valor de intensidade, nesse caso 160>, <valor máximo>, <Tipo>) retorna o img binarizado e um retVal
+    (T, img_bin1) = cv2.threshold(img_sv, 200, 255, cv2.THRESH_BINARY)
+    (T, img_bin2) = cv2.threshold(img_sv, 200,
+                                  255, cv2.THRESH_BINARY_INV)  # Inverso
 
-    # Converte para um frame em tons de cinza
-    frame_pb = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # cv2.cvtColor(<imagem>, <Tipo a converter>)
+    # Detecar bordas no img usando a função Canny
+    # cv2.Canny(<imagem>, <limiar 1>, <limiar2>) => Se x > limiar 2 = borda; se x < limiar 1 = não borda; Se  20 < x < 120 = depende
+    bordas = cv2.Canny(img_bin1, 50, 150, apertureSize=5)
+    cv2.imshow("bordas", bordas)
+    # Aplica a função goodFeaturesToTrack
+    corners = cv2.goodFeaturesToTrack(bordas, 100, 0.5, 10)
+    nome = ''
 
-    # Suaviza ou borra o frame e tira o ruído dele. Aumenta a precisão das bordas     Sempre ímpares
-    frame_sv = cv2.GaussianBlur(frame_pb, (3, 3),
-                                0)  # cv2.GaussianBlur(<imagem>, (<largura>, <altura>), <qtd de desvios no eixo X e Y>)
+    # 100 = número de cantos
+    # 0.1 = qualidade (0 a 1)
+    # 10 = distancia minima entre cantos (que são detectados)
+    if corners is not None:
+        corners = np.int0(corners)
+        if len(corners) == 3:
+            nome = 'Triangulo'
+        elif len(corners) == 4:
+            nome = 'Quadrado'
+        elif len(corners) == 5:
+            nome = 'Pentagono'
+        elif len(corners) == 6:
+            nome = 'Hexagono'
+        for corner in corners:
+            x, y = corner.ravel()
+            cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
 
-    # Binariza o frame fazendo um cálculo das intensidades dos frames, onde pixels proximos de 0 se tornam 0 e os pixels proximos ao 255 ficam 255
-    (T, frame_bin1) = cv2.threshold(frame_sv, 160, 255, cv2.THRESH_BINARY)  # cv2.threshold(<imagem>, <valor de intensidade, nesse caso 160>, <valor máximo>, <Tipo>) retorna o frame binarizado e um retVal
-    (T, frame_bin2) = cv2.threshold(frame_sv, 160, 255, cv2.THRESH_BINARY_INV)  # Inverso
-
-    # Detecar bordas no frame usando a função Canny
-    bordas = cv2.Canny(frame_bin1, 100, 300)  # cv2.Canny(<imagem>, <limiar 1>, <limiar2>) => Se x > limiar 2 = borda; se x < limiar 1 = não borda; Se  20 < x < 120 = depende
-
-    # Cria uma pilha vertical contendo pilhas horizontais, tudo isso para mostrar todas as alterações de uma vez
-    pilha = np.vstack([
-        np.hstack([frame_pb, frame_sv]),
-        np.hstack([frame_bin1, bordas])
-    ])
-
-    # Mostra o frame
-    cv2.imshow("Original", frame)
-    cv2.imshow("Resultado", pilha)
-
-    # Aguarda o usuário digitar a tecla 'q' para quebrar o processo
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+    # Mostra o img
+    cv2.imshow(nome, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
